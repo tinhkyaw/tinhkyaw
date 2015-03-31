5ecd989752b52793a1acb30d6e1de18e7bd39915
Tin Kyaw <tkyaw@netflix.com>
2015-03-30 22:42:19 -0700

    minor improvement

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -6,4 +6,5 @@ brew doctor;
 gem update --system;
 gem update;
 gem cleanup;
+pip install --upgrade pip setuptools;
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install --no-use-wheel --upgrade;
