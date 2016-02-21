0ddbe6d86a8e5c8d7cec398b14b3254a1564253b
Tin H Kyaw <tinhkyaw@gmail.com>
2016-02-21 02:55:38 -0800

    pip packages installed now

--- setup.sh
+++ setup.sh
@@ -2,26 +2,9 @@
 if ! command -v brew &> /dev/null
 then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew install git coreutils python ruby
+  brew install git coreutils mysql python ruby swig
 fi
 DIR=$(dirname "$(greadlink -f "$0")")
-if ! brew list emacs &> /dev/null
-then
-  brew install --with-cocoa --with-glib --with-gnutls --with-imagemagick --with-librsvg --with-mailutils emacs
-fi
-if ! brew list gnuplot &> /dev/null
-then
-  brew install --with-aquaterm --with-cairo --with-latex --with-qt --with-wxmac gnuplot
-fi
-if ! brew list octave &> /dev/null
-then
-  brew install --with-audio --with-gui octave
-fi
-if ! brew list cask &> /dev/null
-then
-  brew install cask
-  cat ${DIR}/packages/casks | xargs brew cask install
-fi
 function install_file() {
   local file_name="${1}"
   local src_dir="${2}"
@@ -48,9 +31,9 @@ do
   install_file ${conf_file} ${DIR}/conf ${HOME}
 done
 mkdir -p ${HOME}/bin
-for scr in emacs cleanup-caskroom.sh update-all.sh
+for script_file in emacs cleanup-caskroom.sh update-all.sh
 do
-  install_file ${scr} ${DIR}/scripts ${HOME}/bin
+  install_file ${script_file} ${DIR}/scripts ${HOME}/bin
 done
 install_file ignored ${DIR}/packages ${HOME}/bin
 mkdir -p ${HOME}/.emacs.d
@@ -59,4 +42,22 @@ if ! diff ${DIR}/conf/paths /etc/paths &> /dev/null
 then
   sudo cp ${DIR}/conf/paths /etc/
 fi
-cat ${DIR}/packages/casks | xargs brew cask install
+if ! brew list emacs &> /dev/null
+then
+  brew install --with-cocoa --with-glib --with-gnutls --with-imagemagick --with-librsvg --with-mailutils emacs
+fi
+if ! brew list gnuplot &> /dev/null
+then
+  brew install --with-aquaterm --with-cairo --with-latex --with-qt --with-wxmac gnuplot
+fi
+if ! brew list octave &> /dev/null
+then
+  brew install --with-audio --with-gui octave
+fi
+if ! brew list cask &> /dev/null
+then
+  brew install cask
+  cat ${DIR}/packages/casks | xargs brew cask install
+fi
+pip install --no-use-wheel --upgrade scipy
+cat ${DIR}/packages/pips | xargs pip install --no-use-wheel --upgrade
