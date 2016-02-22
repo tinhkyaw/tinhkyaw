2c8e1f030ba4b8a8ff51f368a6dd1dd63dc4b935
Tin H Kyaw <tinhkyaw@gmail.com>
2016-02-21 16:20:36 -0800

    completed setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 if ! command -v brew &> /dev/null
 then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew install git coreutils mysql python ruby swig
+  brew install git mysql python swig
+  brew install --with-gmp coreutils
+  brew install --with-doc --with-gdbm --with-gmp --with-libffi ruby
 fi
 DIR=$(dirname "$(greadlink -f "$0")")
 function install_file() {
@@ -46,6 +48,10 @@ if ! brew list emacs &> /dev/null
 then
   brew install --with-cocoa --with-glib --with-gnutls --with-imagemagick --with-librsvg --with-mailutils emacs
 fi
+if ! brew list macvim &> /dev/null
+then
+  brew install --with-lua --with-luajit --with-override-system-vim macvim
+fi
 if ! brew list gnuplot &> /dev/null
 then
   brew install --with-aquaterm --with-cairo --with-latex --with-qt --with-wxmac gnuplot
@@ -54,6 +60,39 @@ if ! brew list octave &> /dev/null
 then
   brew install --with-audio --with-gui octave
 fi
+if ! brew list graphviz &> /dev/null
+then
+  brew install --with-app --with-bindings --with-reetype --with-gts --with-librsvg --with-pango graphviz
+fi
+if ! brew list qcachegrind &> /dev/null
+then
+  brew install --with-graphviz qcachegrind
+fi
+if ! brew list jmeter &> /dev/null
+then
+  brew install --with-plugins jmeter
+fi
+if ! brew list nginx &> /dev/null
+then
+  brew install --with-gunzip --with-libressl --with-passenger --with-spdy --with-webdav nginx
+fi
+if ! brew list node &> /dev/null
+then
+  brew install --with-openssl node
+fi
+if ! brew list geoip &> /dev/null
+then
+  brew install --with-geoipupdate geoip
+fi
+if ! brew list wireshark &> /dev/null
+then
+  brew install --with-libsmi --with-lua --with-portaudio --with-qt --with-qt5 wireshark
+fi
+if ! brew list ffmpeg &> /dev/null
+then
+  brew install --with-dcadec --with-faac --with-fdk-aac --with-ffplay --with-fontconfig --with-freetype --with-frei0r --with-libass --with-libbluray --with-libbs2b --with-libcaca --with-libquvi --with-libsoxr --with-libssh --with-libvidstab --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-openssl --with-opus --with-rtmpdump --with-schroedinger --with-snappy --with-speex --with-theora --with-tools --with-webp --with-x265 --with-zeromq ffmpeg
+fi
+cat ${DIR}/packages/brews | xargs brew install
 if ! brew list cask &> /dev/null
 then
   brew install cask
