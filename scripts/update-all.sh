75470e937c063e924ab000c48e876e949bd486fb
Tin H Kyaw <tinhkyaw@gmail.com>
2016-10-25 19:31:08 -0700

    updated pips, npms

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -53,5 +53,6 @@ gem update --system
 gem update
 gem cleanup
 npm update -g
+npm-check -u -g
 pip install --upgrade pip setuptools
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install --no-binary :all: --upgrade
