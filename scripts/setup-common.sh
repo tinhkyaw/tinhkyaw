31dcd1524b72007f0d230dc6d458f67c817d89b9
Tin Kyaw <tkyaw@tkyaw-mbp143.local>
2017-08-15 03:15:35 -0700

    fixed bugs

--- scripts/setup-common.sh
+++ scripts/setup-common.sh
@@ -1,43 +1,44 @@
 #!/usr/bin/env bash
 function install_file() {
-  local file_name="${1}"
-  local src_dir="${2}"
-  local dst_dir="${3}"
-  local src="${src_dir}/${file_name}"
-  local dst="${dst_dir}/${file_name}"
-  if [ -e ${dst} ]
+  local file_name="$1"
+  local src_dir="$2"
+  local dst_dir="$3"
+  local src="$src_dir/$file_name"
+  local dst="$dst_dir/$file_name"
+  mkdir -p "$dst_dir"
+  if [ -e "$dst" ]
   then
-    if ! diff ${src} ${dst} &> /dev/null
+    if ! diff "$src" "$dst" &> /dev/null
     then
-      mv ${dst} ${dst}.BAK
-      ln -s ${src} ${dst_dir}/
+      mv "$dst" "$dst".BAK
+      ln -s "$src" "$dst_dir"/
     fi
-  elif [ -L ${dst} ]
+  elif [ -L "$dst" ]
   then
-    mv ${dst} ${dst}.BAK
-    ln -s ${src} ${dst_dir}/
+    mv "$dst" "$dst".BAK
+    ln -s "$src" "$dst_dir"/
   else
-    ln -s ${src} ${dst_dir}/
+    ln -s "$src" "$dst_dir"/
   fi  
 }
 function replace_file() {
-  local file_name="${1}"
-  local src_dir="${2}"
-  local dst_dir="${3}"
-  local src="${src_dir}/${file_name}"
-  local dst="${dst_dir}/${file_name}"
-  if [ -e ${dst} ]
+  local file_name="$1"
+  local src_dir="$2"
+  local dst_dir="$3"
+  local src="$src_dir/$file_name"
+  local dst="$dst_dir/$file_name"
+  if [ -e "$dst" ]
   then
-    if ! diff ${src} ${dst} &> /dev/null
+    if ! diff "$src" "$dst" &> /dev/null
     then
-      mv ${dst} ${dst}.BAK
-      cp ${src} ${dst_dir}/
+      mv "$dst" "$dst".BAK
+      cp "$src" "$dst_dir"/
     fi
-  elif [ -L ${dst} ]
+  elif [ -L "$dst" ]
   then
-    mv ${dst} ${dst}.BAK
-    cp ${src} ${dst_dir}/
+    mv "$dst" "$dst".BAK
+    cp "$src" "$dst_dir"/
   else
-    cp ${src} ${dst_dir}/    
+    cp "$src" "$dst_dir"/
   fi  
 }
--- scripts/snapshot.zsh
+++ scripts/snapshot.zsh
@@ -13,12 +13,7 @@ GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
 PKG_DIR="${GIT_ROOT_DIR}/packages"
 brew list > ${SNAPSHOT_DIR}/brew${SUFFIX}.txt
 brew cask list > ${SNAPSHOT_DIR}/cask${SUFFIX}.txt
-if [ "${SUFFIX}" = "w" ]
-then
-  brew cask list > ${PKG_DIR}/min_casks
-else
-  brew cask list > ${PKG_DIR}/casks
-fi
+brew cask list > ${PKG_DIR}/casks
 gem list > ${SNAPSHOT_DIR}/gem${SUFFIX}.txt
 npm ls -g --depth 0 > ${SNAPSHOT_DIR}/npm${SUFFIX}.txt
 pip2 freeze --local > ${SNAPSHOT_DIR}/pip${SUFFIX}.txt
