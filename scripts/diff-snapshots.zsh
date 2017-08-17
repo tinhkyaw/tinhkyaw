c2372b4be365144de5838a5c19a2610377ca1dbb
Tin H Kyaw <tinhkyaw@gmail.com>
2017-08-17 07:34:16 -0700

    updated diff-snapshots.zsh

--- scripts/diff-snapshots.zsh
+++ scripts/diff-snapshots.zsh
@@ -4,12 +4,20 @@ then
   print -P "%F{red}Usage:%f ${0} <suffix-1> <suffix-2>"
   exit 1
 fi
-LHS=$1
-RHS=$2
+SUFFIX1=$1
+SUFFIX2=$2
 SNAPSHOT_DIR="${HOME}/Dropbox/Shared/Snapshots"
 for snapshot in brew cask eclipse gem npm pip
 do
-  print -P "diff %F{blue}${snapshot} ${LHS} ${RHS}%f ..."
-  PREFIX="${SNAPSHOT_DIR}/${snapshot}"
-  diff "${PREFIX}${LHS}.txt" "${PREFIX}${RHS}.txt"
+  FILE1="${snapshot}${SUFFIX1}.txt"
+  FILE2="${snapshot}${SUFFIX2}.txt"
+  LHS="${SNAPSHOT_DIR}/${FILE1}"
+  RHS="${SNAPSHOT_DIR}/${FILE2}"
+  if ! diff "$LHS" "$RHS" &> /dev/null
+  then
+    print -P "%F{red}diff%f %F{blue}${FILE1} ${FILE2}%f"
+    diff "$LHS" "$RHS"
+  else
+    print -P "%F{green}No diff%f for %F{blue}${FILE1} ${FILE2}%f"
+  fi
 done
