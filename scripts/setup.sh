d85770d7a2601ba9af49449e70e29462beec2771
Tin H Kyaw <tinhkyaw@gmail.com>
2019-02-02 02:20:21 -0800

    fixes and improvements

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,42 +5,17 @@ then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 brew tap facebook/fb
-brew install git mysql python ruby swig zsh
-brew install --with-gmp coreutils
+brew install coreutils git mysql python ruby swig zsh
 export SLUGIFY_USES_TEXT_UNIDECODE=yes
 pip3 install -U virtualenvwrapper
 DIR=$(dirname "$(greadlink -f "${0}")")
 cd ${DIR}
 GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
-if ! brew list graphviz &> /dev/null
-then
-  brew install --with-app --with-bindings --with-freetype --with-gts --with-librsvg --with-pango graphviz
-fi
-if ! brew list qcachegrind &> /dev/null
-then
-  brew install --with-graphviz qcachegrind
-fi
-if ! brew list jmeter &> /dev/null
-then
-  brew install --with-plugins jmeter
-fi
-if ! brew list nginx &> /dev/null
-then
-  brew install --with-gunzip --with-passenger --with-webdav nginx
-fi
 if ! brew list node &> /dev/null
 then
   brew install node
   sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
   cat ${GIT_ROOT_DIR}/packages/npms | xargs npm install -g
-fi
-if ! brew list geoip &> /dev/null
-then
-  brew install --with-geoipupdate geoip
-fi
-if ! brew list ffmpeg &> /dev/null
-then
-  brew install --with-faac --with-fdk-aac --wtih-ffplay --with-fontconfig --with-freetype --with-frei0r --with-libass -with-libbluray --with-libbs2b --with-libcaca --with-libsoxr --with-libssh --with-libvidstab --with-libvorbis --withlibvpx --with-opencore-amr --with-openh264 --withopenjpeg --withopenssl --with-opus --with-rtmpdump --withrubberband --with-schroedinger --with-snappy --with-speex --with-theora --with-tools --with-webp --with-x265 --with-xz --with-zeromq ffmpeg
 fi
 brew install cask
 brew tap caskroom/fonts
@@ -54,22 +29,6 @@ for extension in $(cat ${GIT_ROOT_DIR}/packages/vscode_extensions)
 do
   code --install-extension ${extension}
 done
-if ! brew list gnuplot &> /dev/null
-then
-  brew install --with-cairo --with-qt5 --with-tex --with-wxmac gnuplot
-fi
-if ! brew list octave &> /dev/null
-then
-  brew install octave
-fi
-if ! brew list thrift &> /dev/null
-then
-  brew install --with-erlang --with-java --with-libevent --with-python@2 thrift
-fi
-if ! brew list uWSGI &> /dev/null
-then
-  brew install --with-geoip --with-libyaml --with-mono --with-nagios --with-postgresql --with-python --with-ruby --with-zeromq uwsgi
-fi
 export LDFLAGS="-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib/system"
 pip3 install -U cython pyyaml
 pip3 install -U -r ${GIT_ROOT_DIR}/packages/pip3s
