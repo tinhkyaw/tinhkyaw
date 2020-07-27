aa9ccae231b22c004544e9f0dce7d02c317dcfe5
Tin H Kyaw <tinhkyaw@gmail.com>
2020-07-27 13:23:33 -0700

    improved git-pull-all.zsh

--- scripts/git-pull-all.zsh
+++ scripts/git-pull-all.zsh
@@ -12,14 +12,20 @@ do
   cd ${gr}
   if [[ $(git remote) ]]
   then
-    print -P "%F{blue}Attempting%f git pull %F{cyan}${gr}%f"
-    if ! git pull
+    if (cd "$(git rev-parse --show-toplevel)/.." &&
+        git rev-parse --is-inside-work-tree 2>/dev/null) | grep -q true
     then
-      print -P "%F{red}Retrying%f git pull --no-rebase %F{cyan}${gr}%f"
-      git pull --no-rebase
+      print -P "%F{yellow}Skipping%f %F{cyan}${gr}%f - submodule detected!"
+    else
+      print -P "%F{blue}Attempting%f git pull %F{cyan}${gr}%f"
+      if ! git pull
+      then
+        print -P "%F{red}Retrying%f git pull --no-rebase %F{cyan}${gr}%f"
+        git pull --no-rebase
+      fi
     fi
   else
-    print -P "%F{yellow}Skipping%f %F{cyan}${gr}%f - no remote set"
+    print -P "%F{yellow}Skipping%f %F{cyan}${gr}%f - no remote set!"
   fi
 done
 cd ${P}
