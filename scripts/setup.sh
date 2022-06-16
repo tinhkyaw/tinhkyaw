07d3336c47a1ef48a859ed2d8e66ed9e1d6c9d05
Tin Htet Kyaw <tinhkyaw@gmail.com>
2022-06-15 21:47:14 -0700

    updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,20 +5,6 @@ if ! command -v brew &>/dev/null; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
   brew install coreutils
 fi
-# brew install coreutils git mas openjdk python ruby swig
-# pip3 install -U \
-#   pip \
-#   doc8 \
-#   docsend \
-#   pip-tools \
-#   pipdeptree \
-#   pss \
-#   pytest \
-#   safety \
-#   twine \
-#   unidecode \
-#   virtualenvwrapper \
-#   yolk
 CPATH=$(xcrun --show-sdk-path)/usr/include
 export CPATH
 export LDFLAGS="-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib/system"
@@ -26,19 +12,12 @@ export SLUGIFY_USES_TEXT_UNIDECODE=yes
 DIR=$(dirname "$(greadlink -f "${0}")")
 cd "${DIR}"
 GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
-# if ! brew list node &>/dev/null; then
-#   brew install node
-# fi
-# xargs -I {} brew tap {} <"${GIT_ROOT_DIR}"/packages/taps
-# brew install weaveworks/tap/eksctl
-# sudo spctl --master-disable
-# brew install --cask adoptopenjdk adoptopenjdk8 google-chrome java mactex osxfuse xquartz
+xargs -I {} brew tap {} <"${GIT_ROOT_DIR}"/packages/taps
 xargs brew install <"${GIT_ROOT_DIR}"/packages/brews
 xargs brew install --cask <"${GIT_ROOT_DIR}"/packages/casks
 xargs npm install -g <"${GIT_ROOT_DIR}"/packages/npms
 apm install --packages-file "${GIT_ROOT_DIR}"/packages/atom_packages
 xargs -I {} code --install-extension {} <"${GIT_ROOT_DIR}"/packages/vscode_extensions
-# pip3 install -U cython pyyaml
 pip3 install -U -r "${GIT_ROOT_DIR}"/packages/pip3s
 if [ ! -d "$ZSH" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -58,7 +37,6 @@ if [ ! -d "$ZSH" ]; then
   echo 'You may also want to update .gitconfig'
   printf '%s'"${NORMAL}"
   env zsh -l
-  # xargs < packages/poetry_packages poetry add
 fi
 if [ -z "$ZSH" ]; then
   ZSH=~/.oh-my-zsh
