dfe96c31fe80e6ea2689265ac863eeb230a3fa29
Tin Htet Kyaw <tin@tins-air.lan>
2022-06-19 08:14:33 -0700

    updates

--- scripts/update-all.zsh
+++ scripts/update-all.zsh
@@ -53,6 +53,7 @@ fi
 gcloud components update
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+  echo "Updating ~/.zprezto"
   zprezto-update
 fi
 cd ${P}
