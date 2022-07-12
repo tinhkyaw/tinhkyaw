5431e8cdbce5c28651fd28ddd99ca96409e05d93
Tin Htet Kyaw <tinhkyaw@gmail.com>
2022-07-12 16:14:25 -0700

    cleanup

--- arch/bin/find-broken-casks.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-brew list --cask 2>/dev/null | xargs -I{} bash -c 'cask_="{}"; brew audit --cask "${cask_}" 2>&1 1>/dev/null | grep "Hbc::DSL#license" >/dev/null && echo "License error in cask: ${cask_}"'
