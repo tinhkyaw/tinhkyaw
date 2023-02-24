c8a3a10a38ec27624d0c6775b393c6bff8689723
Tin Htet Kyaw <tin@lsvp.com>
2023-02-23 18:55:52 -0800

    Update snapshot.zsh

--- bin/snapshot.zsh
+++ bin/snapshot.zsh
@@ -23,7 +23,8 @@ fluid-synth\
       awk -F ':' '{ print $2 }' |
       tr ' ' '\n' |
       sort -u) \
-    <(brew list --formula --full-name -1 | sort) |
+    <(brew list --formula --full-name -1 |
+      sort) |
     grep -Evi ${BREWS_TO_IGNORE}
 } | sort -u >"${LIST_DIR}/brews.txt"
 brew list --cask >"${SNAPSHOT_DIR}/cask${SUFFIX}.txt"
@@ -64,28 +65,58 @@ gensim|\
 smart-open|\
 tensorflow\
 "
+# p=$(
+#   grep -Fvxf \
+#     <(
+#       "${HOMEBREW_PREFIX}"/bin/pip3 freeze |
+#         cut -d '=' -f1 |
+#         cut -d ' ' -f1 |
+#         xargs "${HOMEBREW_PREFIX}"/bin/pip3 show |
+#         grep -i '^requires:' |
+#         awk -F ': ' '{ print $2 }' |
+#         tr '[:upper:]' '[:lower:]' |
+#         tr ',' '\n' |
+#         gsed -e 's/ //g' |
+#         awk NF |
+#         sort -u
+#     ) \
+#     <(
+#       "${HOMEBREW_PREFIX}"/bin/pip3 freeze |
+#         cut -d '=' -f1 |
+#         cut -d ' ' -f1 |
+#         tr '[:upper:]' '[:lower:]' |
+#         sort -u
+#     )
+# )
+# q=$(
+#   grep -Fvxf \
+#     <(brew info --json=v1 --installed |
+#       jq -r \
+#         'map(select((.dependencies + .build_dependencies +
+#          .test_dependencies)[] | contains("python"))) | .[] .name' |
+#       sort -u) \
+#     <(echo "$p")
+# )
+# d=$(
+#   echo "$q" |
+#     xargs "${HOMEBREW_PREFIX}"/bin/pip3 show |
+#     grep -i '^required-by:' |
+#     grep -in '^required-by: [a-z]' |
+#     cut -d ':' -f1 |
+#     gsed -z 's/\n/d;/g'
+# )
+# {
+#   echo "$q" | gsed -e "$d" |
+#     grep -Evi ${PIPS_TO_IGNORE}
+# } | sort -u >"${LIST_DIR}/pip3s.txt"
+if ((!$ + commands[pipdeptree])); then
+  "${HOMEBREW_PREFIX}"/bin/pip3 install pipdeptree
+fi
 p=$(
-  grep -Fvxf \
-    <(
-      "${HOMEBREW_PREFIX}"/bin/pip3 freeze |
-        cut -d '=' -f1 |
-        cut -d ' ' -f1 |
-        xargs "${HOMEBREW_PREFIX}"/bin/pip3 show |
-        grep -i '^requires:' |
-        awk -F ': ' '{ print $2 }' |
-        tr '[:upper:]' '[:lower:]' |
-        tr ',' '\n' |
-        gsed -e 's/ //g' |
-        awk NF |
-        sort -u
-    ) \
-    <(
-      "${HOMEBREW_PREFIX}"/bin/pip3 freeze |
-        cut -d '=' -f1 |
-        cut -d ' ' -f1 |
-        tr '[:upper:]' '[:lower:]' |
-        sort -u
-    )
+  pipdeptree --json-tree |
+    jq -r 'map(.package_name) | .[]' |
+    tr '[:upper:]' '[:lower:]' |
+    sort -u
 )
 q=$(
   grep -Fvxf \
@@ -96,16 +127,8 @@ q=$(
       sort -u) \
     <(echo "$p")
 )
-d=$(
-  echo "$q" |
-    xargs "${HOMEBREW_PREFIX}"/bin/pip3 show |
-    grep -i '^required-by:' |
-    grep -in '^required-by: [a-z]' |
-    cut -d ':' -f1 |
-    gsed -z 's/\n/d;/g'
-)
 {
-  echo "$q" | gsed -e "$d" |
+  echo "$q" |
     grep -Evi ${PIPS_TO_IGNORE}
 } | sort -u >"${LIST_DIR}/pip3s.txt"
 ("${HOMEBREW_PREFIX}"/anaconda3/bin/conda list \
