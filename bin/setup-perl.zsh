bde9be7e99b56e382ce87faa1c7bf6f6c340e660
Tin Kyaw <tin@lsvp.com>
2024-05-11 13:03:05 -0700

    updates

--- bin/setup-perl.zsh
+++ bin/setup-perl.zsh
@@ -4,7 +4,8 @@ curl -L https://cpanmin.us |
 "${HOMEBREW_PREFIX}"/opt/perl/bin/cpanm \
   App::cpanoutdated File::HomeDir \
   Log::Log4perl \
-  Term::ReadLine::Perl
+  Term::ReadLine::Perl \
+  YAML::Tiny
 md=$(perl -V | grep -E "^[ ]*${HOMEBREW_PREFIX}/lib/perl5/site_perl/" | gsed -e 's/ //g')
 if [[ ! -d "${md}" ]]; then
   print -P "%F{yellow}Warning:%f Creating the missing dir: ${md}"
