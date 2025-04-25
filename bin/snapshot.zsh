50a039cbc5de2aed42813c4a9ded2ecebb6f56a7
Tin Htet Kyaw <tinhkyaw@gmail.com>
2025-04-25 13:29:12 -0700

    Update snapshot.zsh

--- bin/snapshot.zsh
+++ bin/snapshot.zsh
@@ -51,7 +51,7 @@ EOF
   echo "${CASKS_TO_ADD}"
 } | sort -u >"${LIST_DIR}/casks.txt"
 brew tap >"${SNAPSHOT_DIR}/tap${SUFFIX}.txt"
-mas list >"${SNAPSHOT_DIR}/mas${SUFFIX}.txt"
+mas list | sort >"${SNAPSHOT_DIR}/mas${SUFFIX}.txt"
 gem list >"${SNAPSHOT_DIR}/gem${SUFFIX}.txt"
 npm ls -g >"${SNAPSHOT_DIR}/npm${SUFFIX}.txt"
 (npm ls -g -p |
